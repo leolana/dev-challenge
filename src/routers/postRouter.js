b240f181a7377be79cff83e43f37ec0f6ed73b08
Leonardo Lana <leolucas.lana@hotmail.com>
2017-09-10 12:06:53 -0300

    Create integration twitter to api post

--- src/routers/postRouter.js
+++ src/routers/postRouter.js
@@ -1,9 +1,29 @@
 import Router from 'koa-router';
+import Twitter from 'twitter';
 
 const postRouter = new Router();
 
-postRouter.get('/:termo', async (ctx) => {
-  ctx.body = { message: 'some message', termo: ctx.params.termo };
+const twitterClient = new Twitter({
+  consumer_key: 'sT4InFgBUms2PBmAhQNVifMMK',
+  consumer_secret: 'T3MsLJgSRK96EnIf3OwR4nJFu7mpdZlADBewdHShi8QtOf66Iv',
+  access_token_key: '4843073440-lwyRDtg6GSqLxtLJGhnlB9JpEe8r9cyh9ZToIRq',
+  access_token_secret: 'lbjRIx68RPYARqxk1v1GYVJXgSCh8qdtbSy1UoHrmI5uQ',
+});
+
+const searchTwitter = term => new Promise((resolve, reject) => {
+  twitterClient.get('search/tweets', { q: term }, (error, tweets) => {
+    if (error) {
+      reject(error);
+    } else {
+      resolve(tweets);
+    }
+  });
+});
+
+postRouter.get('/:term', async (ctx) => {
+  const [tweets] = await Promise.all([searchTwitter(ctx.params.term)]);
+
+  ctx.body = tweets;
 });
 
 module.exports = postRouter;
